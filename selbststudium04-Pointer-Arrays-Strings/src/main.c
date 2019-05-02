9dcecc66c81429f8a23c4327cda0ab3fd9502306
nambrosini <nico.ambrosini@protonmail.com>
2019-05-02 17:28:42 +0200

    Completed sebstudium 04

--- selbststudium04-Pointer-Arrays-Strings/src/main.c
+++ selbststudium04-Pointer-Arrays-Strings/src/main.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-// wordlist
+static int compare (const void * a, const void * b) {
+    return strcmp (*(const char **) a, *(const char **) b);
+}
+
 
 int main(int argc, char* argv[])
 {
     // read and sort
+    char input[20];
+    char wordlist[100][20];
+    int counter = 0;
+    while (true)
+    {
+        scanf("%s", input);
 
-    (void)printf("\nSortierte Liste:\n");
-    // print wordlist
+        if (strcmp(input, "ZZZ") == 0) {
+            break;
+        }
+
+        strcpy(wordlist[counter], input);
+        counter++;
+    } ;
+
+    int n_array = sizeof(wordlist) / sizeof(char*);
+
+    int i;
+    qsort (wordlist, n_array, sizeof (const char *), compare);
+    for (i = 0; i < n_array; i++) {
+        printf ("%d: %s.\n", i, wordlist[i]);
+    }
     
     return EXIT_SUCCESS;
 }
